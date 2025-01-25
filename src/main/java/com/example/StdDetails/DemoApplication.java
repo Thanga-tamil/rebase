4575c08b6566d26c0b8d139bde1db7d10a37441b
Thanga-tamil <thangatamil1177@gmail.com>
2025-01-25 11:18:43 +0530

    direct to main

--- src/main/java/com/example/StdDetails/DemoApplication.java
+++ src/main/java/com/example/StdDetails/DemoApplication.java
@@ -9,9 +9,24 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
+        String[] strs = {"flowers", "floor", "floks"};
+
 
         System.out.println("hello mate");
 
 	}
+
+    public String findLargeSquenceofChar(String[] strs) {
+
+
+        String firstStr = strs[0];
+
+
+        for (int i = 0; i < firstStr.length(); i++) {
+            
+        }
+
+        return null;
+    }
 
 }
